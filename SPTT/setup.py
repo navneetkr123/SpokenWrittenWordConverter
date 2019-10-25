c5e352510326ddc1eb5e4cfdf288aa40a18d88af
Navneet.kr <40607411+navneetkr123@users.noreply.github.com>
2019-10-25 22:14:41 +0530

    Add files via upload

---
+++ SPTT/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "sptt",
+      version = "0.2",
+      description = "Spoken English To Written English Converter",
+      author = "Navneet Kumar",
+      setup_requires=['wheel'],
+      packages = ["sptt"],
+      install_requires = []
+      )
